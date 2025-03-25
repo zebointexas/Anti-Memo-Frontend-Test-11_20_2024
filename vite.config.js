0d54d8154b44bba4ef8c6d3ba2d3ff05bf3726b2
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2025-03-24 20:04:07 -0700

    update some setting for render web service

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // 让 Render 监听外部访问
-    port: 10000, // 监听 Render 推荐的端口
+    host: "0.0.0.0",
+    port: 10000, // 也可以不写，Render 会自动分配 $PORT
+    allowedHosts: ["anti-memo-frontend-test-11-20-2024.onrender.com"], // ✅ 允许 Render 访问
   },
 });
