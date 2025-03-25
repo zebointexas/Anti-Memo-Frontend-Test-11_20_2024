ae63f3fdbcf469087a5a455f220affe3df79cfa1
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2025-03-24 19:53:41 -0700

    update some setting for render web service

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+ 
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // 让 Render 监听外部访问
+    port: 10000, // 监听 Render 推荐的端口
+  },
+});
